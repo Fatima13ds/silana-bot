21bf05249989a9cff09fe0073afc337f0be9c561
Fatima13ds <fatimasilvana671@gmail.com>
2024-07-21 15:45:37 +0100

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -1,22 +1,17 @@
 import yts from 'yt-search'
 import { youtubedl, youtubedlv2 } from '@bochilteam/scraper-sosmed'
 let handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw ` مثال :\n*.play* sami yusuf`
+  if (!text) throw ` مثال :\n*.song* ماهر زين`
   let res = await yts(text)
   let vid = res.videos[0]
-  await conn.sendMessage(m.chat, { react: { text: "⏳",key: m.key,}
+  await conn.sendMessage(m.chat, { react: { text: "🌍",key: m.key,}
   })  
   if (!vid) throw 'لم يتم العثور عليه، حاول عكس العنوان والمؤلف'
   let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
   const url = 'https://www.youtube.com/watch?v=' + videoId
 let vap = `*〔 Y O U T U B E P L A Y 〕*
 
-*عنوان المقطع:* ${title}
-*رابط المقطع:* ${url}
-*وصف المقطع:* ${description}
-*تاريخ نشره:* ${publishedTime}
-*مدته:* ${durationH}
-*عدد المشاهدات:* ${viewH}`
+*عنوان المقطع:* ${title}`
 
 conn.sendMessage(m.chat, {
 text: vap,
@@ -36,19 +31,17 @@ const link = await yt.audio['128kbps'].download()
 }, 
 mimetype: 'audio/mp4', fileName: `${title}`, contextInfo: { externalAdReply: { showAdAttribution: true,
 mediaType:  2,
-mediaUrl: url,
 title: title,
-body: "© SILANA",
-sourceUrl: url,
+body: "© SASUKE TECH",
 thumbnail: await(await conn.getFile(thumbnail)).data                                                                     
                                                                                                                  }
                        }
   }
   return conn.sendMessage(m.chat, doc, { quoted: m })
 }
-handler.help = ['song','play']
+handler.help = ['song','أغنية']
 handler.tags = ['downloader']
-handler.command = /^song|play$/i
+handler.command = /^song|أغنية$/i
 
 export default handler
 
