736ccdb9de7452c8269c3248a3106414d9d970cc
Fatima13ds <fatimasilvana671@gmail.com>
2024-07-21 17:34:19 +0100

    Create intro.js

---
+++ plugins/intro.js
@@ -0,0 +1,16 @@
+let handler = async (m, { conn, usedPrefix, command }) => {
+		
+			await conn.sendMessage(m.chat, { video: { url: dir[Math.floor(Math.random() * dir.length)] }, caption: `إنضم إلى مجموعتي الخاصة`
+https://chat.whatsapp.com/H7iwUtSGtFZI8q0Xf4tbpz }, { quoted: m })
+	}
+
+handler.help = ['bader']
+handler.tags = ['tools']
+handler.command = /^(intro)$/i
+handler.limit = false
+
+export default handler
+
+const dir = [
+'https://telegra.ph/file/36798b631d09cb2056ff9.mp4',
+]
